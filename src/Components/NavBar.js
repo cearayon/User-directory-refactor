280d35b91f28d2a4b83dee14d2e5ce3b596b7024
Carlo <cearayon@gmail.com>
2022-08-29 09:29:55 -0700

    display single user, added previous and next functionality

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -6,13 +6,7 @@ export default function NavBar(){
 
     return (
       <div>
-        <a href='/#'>Previous</a>
-        <span>
-          <button>Edit</button>
-          <button>Delete</button>
-          <button>New</button>
-        </span>
-        <a href='/#'>Next</a>
+       
       </div>
     );
 }
